c7e2741fa93a7f9f593cc754bc9d3e2307f21e7a
Christian Gerlach <christianrgerlach@gmail.com>
2021-11-04 12:13:58 -0700

    mark across elements

--- sttapp/static/script.js
+++ sttapp/static/script.js
@@ -1,30 +1,32 @@
 $(document).ready(function() {
-    try {
-        let re = new RegExp($("#text_input").val(), 'gim')
-        $(".text_output").markRegExp(re);
-    } catch (error) {
-        //console.error(error)
-    }
+            try {
+                let re = new RegExp($("#text_input").val(), 'gim')
+                $(".text_output").markRegExp(re, {
+                        "acrossElements": true});
+                }
+                catch (error) {
+                    //console.error(error)
+                }
 
-    $(".playback_rate").change(function(event) {
-        $(".playback_rate").val(this.value)
-        var players = $(".audio_player")
+                $(".playback_rate").change(function(event) {
+                    $(".playback_rate").val(this.value)
+                    var players = $(".audio_player")
 
-        for (let i = 0; i < players.length; i++) {
-            players[i].playbackRate = this.value
-        }
+                    for (let i = 0; i < players.length; i++) {
+                        players[i].playbackRate = this.value
+                    }
 
-        if (this.value != 1) { $(".playback_rate_label").text("Rate=" + this.value) }
-    });
+                    if (this.value != 1) { $(".playback_rate_label").text("Rate=" + this.value) }
+                });
 
-    $(".playback_rate_reset").click(function(event) {
-        $(".playback_rate").val(1)
-        var players = $(".audio_player")
+                $(".playback_rate_reset").click(function(event) {
+                    $(".playback_rate").val(1)
+                    var players = $(".audio_player")
 
-        for (let i = 0; i < players.length; i++) {
-            players[i].playbackRate = 1
-        }
+                    for (let i = 0; i < players.length; i++) {
+                        players[i].playbackRate = 1
+                    }
 
-        $(".playback_rate_label").text("Rate")
-    });
-});
+                    $(".playback_rate_label").text("Rate")
+                });
+            });
